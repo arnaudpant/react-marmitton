e7849e9e3ae8c405c6f1ed8d3f7922e6b0e0c817
macbook kal <arnaud.pantenier@gmail.com>
2023-01-21 18:07:19 +0100

    suppression import navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useGlobalContext } from "./context";
-import AppBar from "./components/AppBar/AppBar";
+//import AppBar from "./components/AppBar/AppBar";
 import CardRandom from "./components/card-random/CardRandom";
 import FavoriteBarre from "./components/favorisBar/FavoriteBarre";
 import CardsByLetter from "./components/cards-by-letters/CardsByLetter";
