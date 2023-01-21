c24c47f21a1ed27a53bec1ff9424f2dd0f0439b4
macbook kal <arnaud.pantenier@gmail.com>
2023-01-21 18:55:01 +0100

    ajout footer

--- src/App.js
+++ src/App.js
@@ -7,25 +7,19 @@ import SearchBar from "./components/SearchBar/SearchBar";
 import MenuAffiche from "./components/menuAffiche/MenuAffiche";
 import "./styles/styles.css";
 import TitleBar from "./components/titleBar/TitleBar";
+import Footer from "./components/footer/Footer";
 
 function App() {
     /// ========
     // STATE
     // =========
-    const { listFavorisMeals, menuAffiche, dataByLetter, searchLetter } = useGlobalContext();
-
-
-
-
+    const { listFavorisMeals, menuAffiche, dataByLetter, searchLetter } =
+        useGlobalContext();
 
     // ============
     // COMPORTEMENT
     // ============
-    
 
-
-
-    
     // =========
     // AFFICHAGE
     // =========
@@ -35,33 +29,33 @@ function App() {
 
             {listFavorisMeals.length > 0 && <FavoriteBarre />}
 
-            {menuAffiche !== "" ?
+            {menuAffiche !== "" ? (
                 <MenuAffiche />
-                :
-            <div
-                className={
-                    listFavorisMeals.length > 0
-                        ? "container container-favorite"
-                        : "container"
-                }
-            >
-                <CardRandom />
+            ) : (
+                <div
+                    className={
+                        listFavorisMeals.length > 0
+                            ? "container container-favorite"
+                            : "container"
+                    }
+                >
+                    <CardRandom />
 
-                <SearchBar />
+                    <SearchBar />
 
-                {dataByLetter != null ?
-                    <CardsByLetter /> :
-                    (<div className="msg-error">
-                    Pas de menu avec la lettre{" "}
-                    {searchLetter.toUpperCase()} !<br />
-                    Essayez avec une autre lettre
-                </div>)
-                
-                }
+                    {dataByLetter != null ? (
+                        <CardsByLetter />
+                    ) : (
+                        <div className="msg-error">
+                            Pas de menu avec la lettre{" "}
+                            {searchLetter.toUpperCase()} !<br />
+                            Essayez avec une autre lettre
+                        </div>
+                    )}
 
-            </div>
-
-        }
+                    <Footer />
+                </div>
+            )}
         </>
     );
 }
