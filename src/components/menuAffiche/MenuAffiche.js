5c48b136b82f9e82d0d0832cb2913f43cd222652
macbook kal <arnaud.pantenier@gmail.com>
2023-01-21 18:11:01 +0100

    suppression icon dans menuAffiche

--- src/components/menuAffiche/MenuAffiche.js
+++ src/components/menuAffiche/MenuAffiche.js
@@ -1,6 +1,5 @@
 import { useGlobalContext } from "../../context";
 import { TfiYoutube } from "react-icons/tfi";
-import { AiOutlineHeart } from "react-icons/ai";
 
 const MenuAffiche = () => {
     /// ========
