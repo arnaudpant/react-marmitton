cc29f4c89e6b5dbeebc6fe77800d79c86e069a39
macbook kal <arnaud.pantenier@gmail.com>
2023-01-21 18:05:20 +0100

    suppression navbar pour test

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
     // =========
     return (
         <>
-            <AppBar />
+            {/* <AppBar /> */}
 
             {listFavorisMeals.length > 0 && <FavoriteBarre />}
 
