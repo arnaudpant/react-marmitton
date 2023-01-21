125a45f0d8cdc76cecea73de7c6a8df1c4766c85
macbook kal <arnaud.pantenier@gmail.com>
2023-01-21 18:19:31 +0100

    commit avant push

--- src/components/Appbar/AppBar.js
+++ src/components/Appbar/AppBar.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { GiFoodTruck } from "react-icons/gi";
+//import { GiFoodTruck } from "react-icons/gi";
 
 export default function AppBar (){
 
     return (
         <div className="navbar">
-            <div className="navbar__logo">
+            {/* <div className="navbar__logo">
                 <GiFoodTruck />
-            </div>
+            </div> */}
             <h1 className="navbar__title">React Marmitton</h1>
         </div>
     );
